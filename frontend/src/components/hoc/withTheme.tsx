e0ec9da361bfb6ccc62379203eaf6dc764fc6b79
dmytro.serdiuk <serdiukdmitriy1@gmail.com>
2023-05-27 15:35:51 +0300

    refactoring

--- frontend/src/components/hoc/withTheme.tsx
+++ frontend/src/components/hoc/withTheme.tsx
@@ -1,12 +1,13 @@
 import { useSelector } from "react-redux";
 import { IRootReducer } from "~/types/types";
 import React from "react";
+
 interface IInjectedProps {
     theme: string;
     [prop: string]: any;
 }
 
-const withTheme = <P extends IInjectedProps>(Component: React.ComponentType<P>) => {
+const withTheme = <P extends IInjectedProps>(Component: any) => {
     return (props: Pick<P, Exclude<keyof P, keyof IInjectedProps>>) => {
         const theme = useSelector((state: IRootReducer) => state.settings.theme);
 
