d9f18478b5f210f047e64676c1405918596be868
dmytro.serdiuk <serdiukdmitriy1@gmail.com>
2023-05-27 16:35:52 +0300

    Revert "refactoring"
    This reverts commit e0ec9da361bfb6ccc62379203eaf6dc764fc6b79.

--- frontend/src/components/hoc/withTheme.tsx
+++ frontend/src/components/hoc/withTheme.tsx
@@ -1,13 +1,12 @@
 import { useSelector } from "react-redux";
 import { IRootReducer } from "~/types/types";
 import React from "react";
-
 interface IInjectedProps {
     theme: string;
     [prop: string]: any;
 }
 
-const withTheme = <P extends IInjectedProps>(Component: any) => {
+const withTheme = <P extends IInjectedProps>(Component: React.ComponentType<P>) => {
     return (props: Pick<P, Exclude<keyof P, keyof IInjectedProps>>) => {
         const theme = useSelector((state: IRootReducer) => state.settings.theme);
 
